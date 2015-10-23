b951ddd465fa2ef2fe1a6aa53b4754f097125eae
Shaficks <nouirachafik@gmail.com>
2015-10-24 01:27:15 +0200

    Création des dossiers et fichiers du projet

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/* Master 1 Informatique - Spécialité STL - 2015-2016 Semestre 1 - Algorithmique Avancée 
+ * 
+ * Projet ALGAV : 
+ *      - Implémentation des Arbres de la Briandais et Tries Hybrides
+ *      - Implémentation des Primitives de chaque structure
+ *      - Implémentation de fonctions avancées pour chaque structure
+ *      - Comparaison des résultats et efficacité
+ *
+ * Auteur : Chafik NOUIRA
+ */
+
+
+#include "main.h"
